8da5fc2cb4bb52e18ec06053cf28b30df4690c59
John Rapp <johnfarnes@hotmail.com>
2017-12-12 12:16:17 +0100

    Implemented total_sales

--- app/db.js
+++ app/db.js
@@ -1,17 +1,14 @@
-const data = require('../data.json');
+const getData = () => {
+    return require('../data.json');
+}
 
 const getEmployees = () => {
-    const { employees } = data.carshop;
+    const { employees } = getData().carshop;
     return employees;
 };
 
-const getEmployeeById = (id) => {
-    const { employees } = data.carshop;
-    return employees.find(employee => employee.id === id);
-};
-
 const getCarModels = () => {
-    const { carmodels } = data.carshop;
+    const { carmodels } = getData().carshop;
     return carmodels;
 };
 
@@ -19,22 +16,41 @@ const createCarModel = () => {
     // TODO implement
 };
 
-const getCarModelById = (id) => {
-    const { carmodels } = data.carshop;
-    return carmodels.find(model => model.id === id);
-};
-
 const getTotalSales = () => {
-    const { employees, carmodels, sales } = data.carshop;
+    const { employees, carmodels, sales } = getData().carshop;
+
+    const salesOfEmployees = getSalesOfEmployees(employees, carmodels, sales);
+    
     return employees.map((employee) => {
-        // TODO implement
         return Object.assign(employee, {
-            sales: 0
+            sales: salesOfEmployees[employee.id] || 0
         });
     });
 };
+ 
+// Returns map, mapping from an employee id to their sales number
+// does not guarantee a sales number for every employee, will only
+// include if they've made a sale
+const getSalesOfEmployees = (employees, carmodels, sales) => {
+    const carmodelsById = carmodels.reduce((carmodelsById, model) => {
+        carmodelsById[model.id] = model;
+        return carmodelsById;
+    }, {});
+    
+    const employeeSales = sales.reduce((employeeSales, sale) => {
+        const { employee_id, carmodel_id } = sale;
+        if (!(employee_id in employeeSales)) {
+            employeeSales[employee_id] = 0;
+        }
+        const carmodel = carmodelsById[carmodel_id];
+        employeeSales[employee_id] += carmodel.price;
+        return employeeSales;
+    }, {});
 
+    return employeeSales;
+}
 
+// Public interface
 module.exports = {
     getEmployees,
     getCarModels,
