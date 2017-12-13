8a35ea39470fe1f3db0a5dcebd0c32082fdb6224
John Rapp <johnfarnes@hotmail.com>
2017-12-13 17:42:22 +0100

    Minor comments etc

--- client/src/index.js
+++ client/src/index.js
@@ -4,3 +4,7 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// In package.json there is a hack: "&& rm build/service-worker.js"
+// to remove the service worker, which makes demoing the API difficult
+// Can be solved better with more time
